291a3894b4cfddef730bcf8ab6788cf5c912454e
Alberto Vecina Sánchez <avecina@idealista.com>
2021-01-23 16:09:50 +0100

    *[Application] Implemented Basic Auth

--- src/main/kotlin/filmoteca/RegistrationIds.kt
+++ src/main/kotlin/filmoteca/RegistrationIds.kt
@@ -10,7 +10,10 @@ import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.http.annotation.QueryValue
+import io.micronaut.security.annotation.Secured
+import io.micronaut.security.rules.SecurityRule
 
+@Secured(SecurityRule.IS_AUTHENTICATED)
 @Controller("/${Endpoint.ENDPOINT}/{env}/registrationId")
 class RegistrationIds {
 
--- src/main/kotlin/filmoteca/TokenAuthenticationProvider.kt
+++
@@ -1,4 +0,0 @@
-package filmoteca
-
-class TokenAuthenticationProvider {
-}
